8188d4630691979ae7148c834c2eeee3d55979d5
jasonshao <jasonshao@futunn.com>
2023-12-02 22:08:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='greeting_pkg',
+    name='sj-greeting',
     packages=find_packages(),
     version='0.1.0',
     install_requires=[          # 添加了依赖的 package
